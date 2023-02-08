7b77c5fc8d7b1cc0526f724f5c61652a8dca2231
AghilesMeghari <aghiles.meghari@soprasteria.com>
2023-02-08 17:36:17 +0100

    Refactor : Mise à jour de la methode add

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -3,19 +3,16 @@ package com.exalt.company;
 public class StringCalculatorKata {
 
     int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
+        int result = 0;
         if (!numbers.contains(",")) {
-            return Integer.parseInt(numbers);
-        } else {
-            String[] arrayNumbers = numbers.split(",");
-            int resultSum = 0;
-            for (String number : arrayNumbers) {
-                resultSum += Integer.parseInt(number);
-            }
-            return resultSum;
+            return numbers.isEmpty() ? result : Integer.parseInt(numbers);
         }
-
+        String[] arrayNumbers = numbers.split(",");
+        for (String number : arrayNumbers) {
+            result += Integer.parseInt(number);
+        }
+        return result;
     }
+
+
 }
